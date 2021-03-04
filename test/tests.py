d218a12bf2c2b868cc7a0675aab6b6bf97801568
davidPinaud <pinauddavid@yahoo.fr>
2021-03-04 09:48:47 +0100

    Ajout articles acid&campos A et B

---
+++ test/tests.py
@@ -0,0 +1,68 @@
+from unittest.case import TestCase
+import numpy as np
+import os
+from pylab import *
+import matplotlib.pyplot as plt
+from IPython.core.display import display,HTML
+import math
+import pyAgrum as gum
+import sys
+import unittest
+
+#--temporaire--
+#TODO: changer l'import de bandbLIMID (il faut faire un projet python)
+sys.path.append('/Users/davidpinaud/GitHub/PANDROIDE_InfDiag_BandB/code')
+from bandbLIMID import BranchAndBoundLIMIDInference
+
+
+
+class LimidTestCase(unittest.TestCase):
+
+   
+
+    def setUp(self):
+
+        self.ID1=gum.fastID("A->*B<-C->V->B->$U<-C")
+        self.ID2=gum.fastID("A->*B<-C->V->B->$U<-C")
+        self.ID3=gum.fastID("A->*B<-C->V->B->$U<-C")
+        self.ID4=gum.fastID("A->*B<-C->V->B->$U<-C")
+
+        self.ordre1=[self.ID1.idFromName("B")]
+        self.ordre2=[self.ID2.idFromName("B")]
+        self.ordre3=[self.ID3.idFromName("B")]
+        self.ordre4=[self.ID4.idFromName("B")]
+
+        bnb1=BranchAndBoundLIMIDInference(self.ID1,self.ordre1)
+        bnb2=BranchAndBoundLIMIDInference(self.ID2,self.ordre2)
+        bnb3=BranchAndBoundLIMIDInference(self.ID3,self.ordre3)
+        bnb4=BranchAndBoundLIMIDInference(self.ID4,self.ordre4)
+
+    
+    def test_SIS(self):
+        x=1
+        y=[]
+        self.assertEqual(self.bnb1.SIS(x),y,'SIS 1 incorrect')
+        self.assertEqual(self.bnb2.SIS(x),y,'SIS 2 incorrect')
+        self.assertEqual(self.bnb3.SIS(x),y,'SIS 3 incorrect')
+        self.assertEqual(self.bnb4.SIS(x),y,'SIS 4 incorrect')
+
+    def test_fromIDToMoralizedAncestral(self):
+        x=1
+        MoralizedAncestral_digraph1,alphaXid1,BetaYid1=self.bnb1.fromIDToMoralizedAncestral(x)
+        MoralizedAncestral_digraph2,alphaXid2,BetaYid2=self.bnb2.fromIDToMoralizedAncestral(x)
+        MoralizedAncestral_digraph3,alphaXid3,BetaYid3=self.bnb3.fromIDToMoralizedAncestral(x)
+        MoralizedAncestral_digraph4,alphaXid4,BetaYid4=self.bnb4.SIfromIDToMoralizedAncestral(x)
+
+        
+        #test???
+    def test_graphAuxilliaire(self):
+        pass
+
+    """
+    def test_MEU(self):
+        self.bnb.makeInference()
+        self.assertEqual(self.bnb.MEU(), ...,
+                         'wrong maximum esperance utility')
+    """
+if __name__ == '__main__':
+    unittest.main()
