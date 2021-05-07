1870f79c591cb37b4aba33ed5de90098a7e2d47e
davidPinaud <pinauddavid@yahoo.fr>
2021-05-07 23:33:22 +0200

    Generation Doc, ecriture doc, nettoyage git

--- branchAndBound/andOrGraph.py
+++ branchAndBound/andOrGraph.py
@@ -1,6 +1,6 @@
 import numpy as np
 class andOrGraph():
-    """Class that emulates an And/Or Graph (its really a tree)
+    """Class that emulates an And/Or Graph (in reality its a tree)
     Attributes
     ----------
     ID : pyAgrum.InfluenceDiagram
@@ -142,6 +142,26 @@ class andOrGraph():
         """   
         return self.noeuds
 class chanceNode():
+    """AND node for the And/Or Graph
+    Attributes
+    ----------
+    Id : int
+        the id of this node in the corresponding influence Diagram
+    support : list
+        the domain of this node
+    valeur : float
+        the value of this node calculated during the induction process
+    parent : chanceNode or decisionNode
+        the parent of this node in the andOrGraph
+    probabilitiesPosteriori : dict
+        The posterior probabilities calculated during the induction process ; key=a domainValue of this node value: a float
+    contexte : dict
+        The instanciation context of this node ; key = id of a node in the influence diagram, value = the instanciation value of said node
+    id_andOr : int
+        the id of this node in the andOrGraph
+    childs : dict
+        children of this node ; key=domainValue, value=chanceNode or decisionNode
+    """
     def __init__(self,Id,support,parent,valeurParent,contexte,id_andOr):
         self.Id=Id
         self.support=support #le support du noeud
@@ -155,8 +175,21 @@ class chanceNode():
     def __eq__(self, other):
         return self.id_andOr==other.getId_andOr()
     def getParent(self):
+        """parent of the node
+
+        Returns
+        -------
+        chanceNode or decisionNode
+            the parent of the node
+        """
         return self.parent
     def getId_andOr(self):
+        """
+        Returns
+        -------
+        int
+            the id of the node in the AND/OR Graph
+        """
         return self.id_andOr
     def setParent(self,parent):
         self.parent=parent
@@ -165,27 +198,63 @@ class chanceNode():
     def setValeurParent(self,valeurParent):
         self.valeurParent=valeurParent
     def getSupport(self):
+        """
+        Returns
+        -------
+        list
+            domain of the node
+        """ 
         return self.support
     def setSupport(self,support):
         self.support=support
-    def getContexte(self):  
+    def getContexte(self):
+        """The instanciation context of this node
+
+        Returns
+        -------
+        dict
+            key = id of a node in the influence diagram, value = the instanciation value of said node
+        """  
         return self.contexte
     def getNodeID(self):
+        """
+        Returns
+        -------
+        int
+            the id of the node in the influence diagram
+        """
         return self.Id
     def setValeur(self,valeur):
         self.valeur=valeur
     def getValeur(self):
+        """the value of the chance node calculated in the induction process
+
+        Returns
+        -------
+        float
+            value of the chance node
+        """        
         return self.valeur
     def addChild(self,valeurSupportPourCetEnfant,child):
-        """
-        ajoute un enfant avec son identifiant
-        """
         self.childs[valeurSupportPourCetEnfant]=child
     def getChilds(self):
+        """childs of the node in the And/Or Graph
+        Returns
+        -------
+        dict
+            key=domainValue, value=chanceNode or decisionNode
+        """
         return self.childs
     def setChilds(self,childs):
         self.childs=childs
     def getProbabilitiesPosteriori(self):
+        """The posterior probabilities calculated during the induction process
+
+        Returns
+        -------
+        dict
+            key=a domainValue of this node value: a float
+        """        
         return self.probabilitiesPosteriori
     def setProbabilitiesPosteriori(self,probabilitiesPosteriori):
         self.probabilitiesPosteriori=probabilitiesPosteriori
@@ -193,6 +262,34 @@ class chanceNode():
 #parent_decision, c'est le noeud de décision du quel est issue ce noeud de décision dans le graphe ET/OU
 #contexte est l'instanciation des parents_chance
 class decisionNode():
+    """OR node for the And/Or Graph
+    Attributes
+    ----------
+    Id : int
+        the id of this node in the corresponding influence Diagram
+    support : list
+        the domain of this node
+    decisionOptimale : any
+        the optimal decision of this node
+    ValeurDecisionOptimale : float
+        the value of the optimal decision of this node calculated during the induction process
+    parent : chanceNode or decisionNode
+        the parent of this node in the andOrGraph
+    contexte : dict
+        The instanciation context of this node ; key = id of a node in the influence diagram, value = the instanciation value of said node
+    id_andOr : int
+        the id of this node in the andOrGraph
+    doNotDevelop : list
+        list of domain values that should not be developped (its upper bound is smaller than the evaluation of another branch corresponding to a domain value of this node)
+    inference : pyAgrum.ShaferShenoyLIMIDInference
+        the inference object on which we used to calculated the value of the node (only if this node is a leaf)
+    enfants : dict
+        children of this node ; key=domainValue, value=chanceNode or decisionNode
+    evaluation : dict
+        evaluation of this node ; key=domainValue, value=(mean,variance)
+    borneSup : dict
+        upper bounds for the branches of this node (one for each domain value and only if this node is a leaf), key=domainValue, value=(mean,variance) ; 
+    """
     def __init__(self,Id,contexte,parent,support,id_andOr):
         self.id_andOr=id_andOr
         self.contexte=contexte#key= id du noeud (dans le diagramme d'influence, 
@@ -213,14 +310,34 @@ class decisionNode():
     def __eq__(self, other):
         return self.id_andOr==other.getId_andOr()
     def getParent(self):
+        """parent of the node
+
+        Returns
+        -------
+        chanceNode
+            the parent of the node
+        """
         return self.parent
-    def getID(self):
+    def getID(self):        
         return self.Id
     def setInference(self,inference):
         self.inference=inference
     def getInference(self):
+        """returns the Shafer Shenoy Object used to make the inference on this node (only if it is a leaf node)
+
+        Returns
+        -------
+        pyAgrum.ShaferShenoyLIMIDInference
+            the inference object
+        """        
         return self.inference
     def getId_andOr(self):
+        """
+        Returns
+        -------
+        int
+            the id of the node in the AND/OR Graph
+        """   
         return self.id_andOr
     def isProcessed(self):
         return len(self.doNotDevelop)+len(self.evaluation)==len(self.support)
@@ -229,25 +346,54 @@ class decisionNode():
     def addDoNotDevelop(self,domainValue):
         self.doNotDevelop.append(domainValue)
     def getDecisionOptimale(self):
+        """Returns the optimal decision for this node
+
+        Returns
+        -------
+        any
+            the optimal decision of this node, a value of its domain
+        """        
         return self.decisionOptimale
     def setDecisionOptimale(self,decisionOptimale):
         self.decisionOptimale=decisionOptimale
     def getValeurDecisionOptimale(self):
+        """return the value of the optimal decision
+
+        Returns
+        -------
+        float
+            the value
+        """        
         return self.ValeurDecisionOptimale
     def setValeurDecisionOptimale(self,decisionOptimale):
         self.ValeurDecisionOptimale=decisionOptimale
     def getSupport(self):
+        """
+        Returns
+        -------
+        list
+            domain of the node
+        """        
         return self.support
     def setSupport(self,support):
         self.support=support
     def getEnfants(self):
+        """
+        Returns
+        -------
+        dict
+            key=domainValue, value=chanceNode or decisionNode
+        """        
         return self.enfants
     def getNodeID(self):
+        """
+        Returns
+        -------
+        int
+            the id of the node in the influence diagram
+        """
         return self.Id    
     def addEnfant(self,enfant,valeur):
-        """
-        ajoute un enfant avec son identifiant
-        """
         self.enfants[valeur]=enfant
     def addEnfantProcessed(self,enfantID):
         self.enfantProcessed.append(enfantID)
@@ -256,15 +402,43 @@ class decisionNode():
     def setContexte(self,contexte):
         self.contexte=contexte
     def getContexte(self):
+        """The instanciation context of this node
+
+        Returns
+        -------
+        dict
+            key = id of a node in the influence diagram, value = the instanciation value of said node
+        """        
         return self.contexte
     def getParent(self):
+        """parent of the node
+
+        Returns
+        -------
+        chanceNode
+            the parent of the node
+        """        
         return self.parent
     def addBorneSup(self,key,borneSup):
         self.borneSup[key]=borneSup
     def getBorneSup(self):
+        """Getter function for the upper valuation of the node (only is not a leaf)
+
+        Returns
+        -------
+        dict
+            key=domainValue, value=(mean,variance)
+        """        
         return self.borneSup
     def addEvaluation(self,key,evaluation):
         self.evaluation[key]=evaluation
     def getEvaluation(self):
+        """Getter function for the valuation of the node
+
+        Returns
+        -------
+        dict
+            key=domainValue, value=(mean,variance)
+        """  
         return self.evaluation
 
--- tests/tests.py
+++ tests/tests.py
@@ -16,34 +16,34 @@ sys.path.append("/Users/davidpinaud/GitHub/PANDROIDE_InfDiag_BandB/code/exemple_
 from exemple_robot import createRandomID
 
 class LimidTestCase(unittest.TestCase):
-    # def setUpTestFigures(self,graph,src,dst,model=None,pdfname=None):
+    def setUpTestFigures(self,graph,src,dst,model=None,pdfname=None):
 
-    #     dsep = MinimalDSep(graph,model)
-    #     x=src
-    #     y=dst
-    #     return dsep.find(x, y)
+        dsep = MinimalDSep(graph,model)
+        x=src
+        y=dst
+        return dsep.find(x, y)
 
 
-    # def testFigures_seperating_set(self):
-    #     self.assertTrue(self.setUpTestFigures(*figure1())=={4})
-    #     self.assertTrue(self.setUpTestFigures(*figure6())=={4, 5})
-    #     self.assertTrue(self.setUpTestFigures(*ID_Simple_Test())=={3})
+    def testFigures_seperating_set(self):
+        self.assertTrue(self.setUpTestFigures(*figure1())=={4})
+        self.assertTrue(self.setUpTestFigures(*figure6())=={4, 5})
+        self.assertTrue(self.setUpTestFigures(*ID_Simple_Test())=={3})
 
-    # def test_seperating_set(self):
-    #     gen=gum.BNGenerator()
-    #     for n in [10,30,50]:
-    #         rand=random.sample(list(range(130,171)),n)
-    #         const=[(rand[i]/100)*n for i in range(10)]
-    #         for l in range(10):
-    #             bn=gen.generate(n_nodes=n,n_arcs=int(const[l]),n_modmax=4)
-    #             #src,dst=npRandom.choice(list(bn.nodes()),replace=False,size=2)
-    #             l=random.sample(bn.nodes(),2)
-    #             src=l[0]
-    #             dst=l[1]
-    #             graph=bn.moralizedAncestralGraph({src,dst})
-    #             m=MinimalDSep(graph,bn)
-    #             separatingSet=m.find(src,dst)
-    #             self.assertTrue(bn.isIndependent(src,dst,list(separatingSet)))
+    def test_seperating_set(self):
+        gen=gum.BNGenerator()
+        for n in [10,30,50]:
+            rand=random.sample(list(range(130,171)),n)
+            const=[(rand[i]/100)*n for i in range(10)]
+            for l in range(10):
+                bn=gen.generate(n_nodes=n,n_arcs=int(const[l]),n_modmax=4)
+                #src,dst=npRandom.choice(list(bn.nodes()),replace=False,size=2)
+                l=random.sample(bn.nodes(),2)
+                src=l[0]
+                dst=l[1]
+                graph=bn.moralizedAncestralGraph({src,dst})
+                m=MinimalDSep(graph,bn)
+                separatingSet=m.find(src,dst)
+                self.assertTrue(bn.isIndependent(src,dst,list(separatingSet)))
 
     def test_branchAndBound_IDAleatoires(self):
         for i in range(10):
